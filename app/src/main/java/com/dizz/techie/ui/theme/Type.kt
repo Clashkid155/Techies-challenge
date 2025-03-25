0c4e826555b418c766729b736d0b47f92ab2860f
Clashkid155 <beluchukwunjike@gmail.com>
2025-03-25 10:15:32 +0100

    Use poppins font
    Signed-off-by: Clashkid155 <beluchukwunjike@gmail.com>

--- app/src/main/java/com/dizz/techie/ui/theme/Type.kt
+++ app/src/main/java/com/dizz/techie/ui/theme/Type.kt
@@ -2,19 +2,40 @@ package com.dizz.techie.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dizz.techie.R
+
+
+// Custom fonts
+private val poppinsFont = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
+)
+private val defaultTypography = Typography()
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = poppinsFont),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = poppinsFont),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = poppinsFont),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = poppinsFont),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = poppinsFont),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
