ae0bc54701687ed42e7553be0643b400496b2c95
Clashkid155 <beluchukwunjike@gmail.com>
2025-03-28 02:49:28 +0100

    Create a custom button that can scale when tapped.
    Signed-off-by: Clashkid155 <beluchukwunjike@gmail.com>

--- app/src/main/java/com/dizz/techie/ui/CommonComposable.kt
+++ app/src/main/java/com/dizz/techie/ui/CommonComposable.kt
@@ -1,11 +1,19 @@
 package com.dizz.techie.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.dizz.techie.ui.theme.ActiveComponentColor
@@ -27,5 +35,28 @@ fun YellowButton(
         )
     ) {
         Text(buttonText, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun ScalableYellowButton(modifier: Modifier = Modifier, enabled: Boolean) {
+
+    val (containerColor, textColor) = when (enabled) {
+        true -> Pair(ActiveComponentColor, Color.Black)
+        false -> Pair(Color(0xFF34312F), Color.White)
+    }
+    Box(
+        modifier
+            .background(containerColor, ButtonDefaults.elevatedShape)
+            .padding(ButtonDefaults.ContentPadding)
+            .semantics { role = Role.Button }, Alignment.Center
+    ) {
+        Text(
+            "Continue",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = textColor,
+            style = MaterialTheme.typography.labelLarge
+        )
     }
 }
